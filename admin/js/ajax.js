f0fd5f0d4710bd6c8ca05f37bb00e3dadf947627
Kevin <luoqijin@163.com>
2019-12-28 17:21:34 +0800

    封装

--- admin/js/ajax.js
+++ admin/js/ajax.js
@@ -1,15 +1,17 @@
-(function(window){
-   const token = localStorage.getItem('token');
-   $.ajax({
-     beforsend(xhr){
-        if(!token){
-        location.href ='./login.html';
-        }
-        // 如果不是./login.html 登录页就统一添加请求头
-         if(location.href.indexOf('./login.html') === -1){
-        //  xhr为原生的对象  所以用原生方法添加请求头
-            xhr.setRequestHeader(" Authorization",token);
-        }
+(function(window) {
+  const token = localStorage.getItem("token");
+  $.ajaxSetup({
+    beforeSend(xhr) {
+      if (!token) {
+        location.href = "./login.html";
       }
-    })
+      // debugger
+      // 如果不是./login.html 登录页就统一添加请求头
+      if (location.href.indexOf("./login.html") === -1) {
+        //  xhr为原生的对象  所以用原生方法添加请求头
+        // debugger
+        xhr.setRequestHeader("Authorization", token);
+      }
+    }
+  });
 })(window);
