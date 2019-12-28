37c507a332d0024f7fd7d9f5d8fd4ce2ab11e443
Kevin <luoqijin@163.com>
2019-12-28 16:05:28 +0800

    优化

--- admin/js/ajax.js
+++ admin/js/ajax.js
@@ -6,7 +6,7 @@
         location.href ='./login.html';
         }
         // 如果不是./login.html 登录页就统一添加请求头
-         if(location.href.indexOf('./login.html')=== -1){
+         if(location.href.indexOf('./login.html') === -1){
         //  xhr为原生的对象  所以用原生方法添加请求头
             xhr.setRequestHeader(" Authorization",token);
         }
